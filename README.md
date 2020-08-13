d8b35ca1660924a3b40d477fe09007b2ed0de367
blipp <bclipp770@gmail.com>
2020-08-12 20:03:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# games_snake_python
+## This repo is for a basic snake game I am making for my kids in python.
---
+++ __main__.py
@@ -0,0 +1,10 @@
+import turtle
+
+
+def main():
+    win = turtle.Screen()
+    win.title("Shimmy's snake game")
+
+
+if __name__ == "__main__":
+    main()

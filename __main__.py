f7258c72ffe0ff30bb7c09abff0c06113684c6ad
blipp <bclipp770@gmail.com>
2020-08-13 20:48:34 -0400

    add more features

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import turtle
 import time
-from random import random
+from random import randint
 
 
 def move():
@@ -41,11 +41,19 @@ def go_left():
         head.direction = "left"
 
 
+# holds body
+segments = []
+DELAY = 0.15
+SCORE = 0
+HIGH_SCORE = 0
+
+# basic board
 win = turtle.Screen()
-win.title("Kalgi's snake game")
+win.title("Shimmy's game")
 win.bgcolor("blue")
-win.setup(width=600, height=600)
+win.setup(width=700, height=700)
 win.tracer(0)
+# snake's head
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -53,60 +61,21 @@ head.color("black")
 head.penup()
 head.goto(0, 100)
 head.direction = "stop"
-delay=0.1
+# setup keyboard
 win.listen()
 win.onkey(go_up, "w")
 win.onkey(go_down, "s")
 win.onkey(go_right, "d")
 win.onkey(go_left, "a")
+# setup food
 food = turtle.Turtle()
 food.speed(0)
-food.shape("circle")
-food.color("red")
+food.shape("triangle")
+food.color("green")
 food.penup()
-food.shapesize(0.50, 0.50)
-food.goto(0, 0)
-if head.distance(food) <15:
-    x = random.randint(-290, 290)
-    y = random.randint(-290, 290)
-    food.goto(x, y)
-segments = []
-new_segment = turtle.Turtle()
-new_segment.speed(0)
-new_segment.shape("square")
-new_segment.color("grey")
-new_segment.penup()
-segments.append(new_segment)
-for index in range(len(segments)-1, 0, -1):
-        x = segments[index-1].xcor()
-        y = segments[index-1].ycor()
-        segments[index].goto(x, y)
-if len(segments) > 0:
-        x = head.xcor()
-        y = head.ycor()
-        segments[0].goto(x, y)
-if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
-        time.sleep(1)
-        head.goto(0, 0)
-        head.direction = "stop"
-
-for segment in segments:
-    segment.goto(1000, 1000)
-
-segments = []
-
-for segment in segments:
-    if segment.distance(head) < 20:
-        time.sleep(1)
-        head.goto(0, 0)
-        head.direction = "stop"
-
-        # Hide the segments
-        for segment in segments:
-            segment.goto(1000, 1000)
-
-        # clear segment list
-        segment.clear()
+food.shapesize(1.70, 1.70)
+food.goto(0, 1)
+# score
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
@@ -114,16 +83,41 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-high_score  = 100
-pen.write("Score: 0 High Score: {}".format(high_score), align="center", font=("Courier", 24, "normal"))
-score = 0
-high_score = 0
-score = score + 10
+pen.write("Score: 0 High Score: {}"
+          .format(HIGH_SCORE), align="center", font=("Courier", 24, "normal"))
 
-if score > high_score:
-    high_score = score
-
+# game loop
 while True:
     win.update()
     move()
-    time.sleep(delay)
+    # setup eating
+    if head.distance(food) < 15:
+        x = randint(-290, 290)
+        y = randint(-290, 290)
+        food.goto(x, y)
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+        SCORE = SCORE + 10
+        if SCORE > HIGH_SCORE:
+            HIGH_SCORE = SCORE
+        pen.clear()
+        pen.write("score: {} High Score: {}".format(SCORE, HIGH_SCORE), align="center", font=("Courier", 24, "normal"))
+    if head.xcor() > 400 or head.xcor() < -400 or head.ycor() > 400 or head.ycor() < -400:
+        segments = []
+        head.goto(0, 0)
+        head.direction = "stop"
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+    # fix first body
+    if len(segments) > 0:
+        x = head.xcor() + 1
+        y = head.ycor() + 1
+        segments[0].goto(x, y)
+
+    time.sleep(DELAY)
